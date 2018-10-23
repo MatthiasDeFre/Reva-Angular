d06a48e05576cc9af0434ac7afa790ace7d4c6ab
Jelle <geers.jelle@gmail.com>
2018-10-23 12:04:10 +0200

    pushing before making new branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     TeacherModule,
     AppRoutingModule,
     FontAwesomeModule
- 
+
   ],
   providers: [basehttpInterceptorProviders],
   bootstrap: [AppComponent]
